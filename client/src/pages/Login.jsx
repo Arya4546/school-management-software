0d6ea6557629add840d70992941ca2ac81353cd8
Arya Deep Singh <singharya9693@gmail.com>
2025-06-13 13:15:49 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { motion } from 'framer-motion';
 import { useNavigate } from 'react-router-dom';
-import logo from '../assets/logo.png';
+import logo from '../assets/logo.jpg';
 import PropTypes from 'prop-types';
 
 const Login = ({ onLogin }) => {
